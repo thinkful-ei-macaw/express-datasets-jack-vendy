199ab63c79a53dd6b6160710277fb429269e9d57
Jack Lansing <jack.lansing@live.com>
2020-03-24 17:04:38 -0400

    avg_vote filter logic added

--- server.js
+++ server.js
@@ -5,30 +5,32 @@ const morgan = require('morgan');
 const movies = require('./movies-data-small.json');
 const app = express();
 
-
 app.use(morgan('dev'));
 
-app.get('/movie', (req,res)=>{
+app.get('/movie', (req, res) => {
   let moviesList = [...movies];
-  let { genre, country}   = req.query;
-  
+  let { genre, country, avg_vote } = req.query;
 
-  if(genre){
-    genre=genre.toLowerCase();
-    moviesList = moviesList.filter(movie =>{
-      console.log(movie.genre.toLowerCase(), genre);
+  if (genre) {
+    genre = genre.toLowerCase();
+    moviesList = moviesList.filter(movie => {
       return movie.genre.toLowerCase().includes(genre);
     });
   }
-  if(country){
-    country=country.toLowerCase();
-    moviesList = moviesList.filter(movie =>{
+  if (country) {
+    country = country.toLowerCase();
+    moviesList = moviesList.filter(movie => {
       return movie.country.toLowerCase().includes(country);
     });
   }
+
+  if (avg_vote) {
+    avg_vote = +avg_vote;
+    moviesList = moviesList.filter(movie => movie.avg_vote >= avg_vote);
+  }
+
   res.json(moviesList);
 });
-
 
 const PORT = 8000;
 
