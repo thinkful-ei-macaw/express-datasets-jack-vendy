bc1ab709b50165f9bccc13bea69cebcea5ce012d
Jack Lansing <jack.lansing@live.com>
2020-03-24 17:15:16 -0400

    added helmet and cors for better security

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
-
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 const movies = require('./movies-data-small.json');
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
 app.use(validateBearerToken);
 
