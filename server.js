e36326cf59a4a2d5a9c4f0aa5f9b4a06289ccc14
Vendy Prum <vendy@thinkful.com>
2020-03-24 13:49:31 -0700

    genre functions

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-console.log('hello')
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const movies = require('./movies-data-small.json');
+const app = express();
+
+
+app.use(morgan('dev'));
+
+app.get('/movie', (req,res)=>{
+  let moviesList = [...movies];
+  let genre = req.query.genre.toLowerCase();
+
+  if(genre){
+    moviesList = moviesList.filter(movie =>{
+      console.log(movie.genre.toLowerCase(), genre);
+      return movie.genre.toLowerCase().includes(genre);
+    });
+  }
+  res.json(moviesList);
+});
+
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
